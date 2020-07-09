fdee592bfa72832252360093c36fcc4c72c24003
howardyangyixuan <howardyangyixuan@gmail.com>
2020-07-09 21:20:46 +0800

    查看脚本是否成功运行

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import x from "./x.js";
 import png from "./assets/1.png";
 console.log(png);
 const div = document.getElementById("app");
-div.innerHTML = `<img src=${png}>`;
+// div.innerHTML = `<img src=${png}>`;
 
 const button = document.createElement("button");
 button.innerText = "懒加载";
